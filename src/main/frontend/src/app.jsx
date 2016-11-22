5171c220979bcf4de8c1396b414e718e6a32d747
Igor Slotin <igorslotin@gmail.com>
2016-11-22 21:29:19 +0400

    Working on login

--- src/main/frontend/src/app.jsx
+++ src/main/frontend/src/app.jsx
@@ -1,50 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, Link, hashHistory ,browserHistory } from 'react-router'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {AppBar} from 'material-ui';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-import FlatButton from 'material-ui/FlatButton';
 
 
 import Hello from './components/hello.jsx';
 import Empty from './components/empty.jsx';
-import Login from './components/login.jsx';
-
-
-const Logged = <FlatButton label="Login" onClick={()=>{
-    hashHistory.push('/login');
-}}></FlatButton>;
-
-class App extends React.Component{
-    render() {
-        return (
-            <div>
-                <MuiThemeProvider>
-                    <AppBar title="Car sell"
-                            iconElementLeft={<div/>}
-                            iconElementRight={Logged}
-                    />
-                </MuiThemeProvider>
-                <h1>App</h1>
-                <ul>
-                    <li><Link to="/hello">Hello</Link></li>
-                    <li><Link to="/empty">Empty</Link></li>
-                </ul>
-                {this.props.children}
-            </div>
-        )
-    }
-}
-
+import Login from './components/login/index.jsx';
+import Main from './components/main/index.jsx';
 
 render((
     <Router history={hashHistory}>
-        <Route path="/" component={App}>
-            <Route path="hello" component={Hello} />
-            <Route path="empty" component={Empty} />
-            <Route path="login" component={Login} />
+        <Route path="/" component={Main}>
+            <Route path="/empty" component={Empty} />
+            <Route path="/login" component={Login} />
+            <Route path="*" component={Hello} />
         </Route>
     </Router>
 ), document.getElementById('app'));
---
+++ src/main/frontend/src/components/login/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Paper from 'material-ui/Paper';
+import MTP from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import BackButton from 'material-ui/svg-icons/navigation/arrow-back';
+import { hashHistory } from 'react-router';
+
+const style = {
+    mainBlock: {
+        padding: '2em',
+        margin: "0 auto",
+        maxWidth: "500px",
+        minWidth: "100px",
+        textAlign: "center"
+    },
+    TextFieldStyle: {
+        "marginBottom": "30px"
+    },
+    PaperStyle: {
+        width: "100%",
+        height: "100%"
+    },
+    BackButtonStyle: {
+        margin: "40px 40px"
+    }
+};
+
+class Login extends React.Component {
+    render() {
+        return <MTP>
+            <Paper style={style.PaperStyle}>
+                <FloatingActionButton style={style.BackButtonStyle} onClick={()=> hashHistory.goBack()}>
+                    <BackButton />
+                </FloatingActionButton>
+                <div style={style.mainBlock}>
+                    <TextField style={style.TextFieldStyle}
+                        hintText="User name"
+                    />
+                    <br/>
+                    <TextField style={style.TextFieldStyle}
+                               type="password"
+                                hintText="Password"
+                    />
+                    <RaisedButton label="LOGIN" fullWidth={true} />
+                </div>
+            </Paper>
+        </MTP>;
+    }
+}
+
+export default Login;
